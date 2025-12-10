d8891bdf892a429d0143a514ca9297b8306138d1
yjp <1773702740@qq.com>
2025-12-10 21:57:27 +0800

    封装Redis工具类

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,20 +1,31 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
+import com.hmdp.utils.RedisConstants;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.TimeUnit;
+
+import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
+
 @SpringBootTest
 class HmDianPingApplicationTests {
+
+    @Resource
+    private CacheClient cacheClient;
 
     @Resource
     private ShopServiceImpl shopServiceImpl;
 
     @Test
     void testSaveShop() throws InterruptedException {
-        shopServiceImpl.saveShop2Redis(1L, 10L);
+        Shop shop = shopServiceImpl.getById(1L);
+        cacheClient.setWithLogicalEXpire(CACHE_SHOP_KEY + 1L, shop, 10L, TimeUnit.SECONDS);
     }
 
 }
