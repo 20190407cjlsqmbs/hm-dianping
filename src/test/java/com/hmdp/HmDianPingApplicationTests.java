11a592248c8d2f82d2d65b4a6ba155825ec0a027
yjp <1773702740@qq.com>
2025-12-11 14:58:58 +0800

    添加优惠卷

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -4,11 +4,15 @@ import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
 import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.RedisConstants;
+import com.hmdp.utils.RedisIdWorker;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 import static com.hmdp.utils.RedisConstants.CACHE_SHOP_KEY;
@@ -21,6 +25,37 @@ class HmDianPingApplicationTests {
 
     @Resource
     private ShopServiceImpl shopServiceImpl;
+
+    @Resource
+    private RedisIdWorker redisIdWorker;
+
+    private ExecutorService es = Executors.newFixedThreadPool(500);
+
+    @Test
+    void testIdWorker() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(300);
+        Runnable task = () -> {
+            for (int i = 0; i < 100; i++) {
+                Long id = redisIdWorker.nextId("order");
+                System.out.println("id:" + id);
+            }
+            latch.countDown();
+        };
+        long begin = System.currentTimeMillis();
+        for (int i = 0; i < 300; i++) {
+            es.submit(task);
+        }
+
+        latch.await();
+        es.shutdown(); // 优雅地关闭线程池，等待所有任务结束
+
+        // 3. 阻塞当前线程，等待线程池中的所有线程在指定时间内（如60秒）终止
+        // 如果超时，它会返回 false，但通常情况下会成功终止。
+        es.awaitTermination(60, TimeUnit.SECONDS);
+        long end = System.currentTimeMillis();
+        System.out.println("time:" + (end - begin));
+
+    }
 
     @Test
     void testSaveShop() throws InterruptedException {
